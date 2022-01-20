bf8f5d500bf71426a5e287f517ba814c0caeb53d
Benjamin Bischoff <benjamin.bischoff@trivago.com>
2022-01-20 12:12:12 +0100

    conditional complexity example

---
+++ src/main/java/blog/softwaretester/ooabusers/conditionalcomplexity/Vehicles.java
@@ -0,0 +1,14 @@
+package blog.softwaretester.ooabusers.conditionalcomplexity;
+
+public class Vehicles {
+    public int numberOfWheels(String vehicle, int numberOfVehicles) throws Exception {
+
+        return switch (vehicle) {
+            case "car" -> 4 * numberOfVehicles;
+            case "boat" -> 0;
+            case "bike" -> 2 * numberOfVehicles;
+            case "bicycle" -> 2 * numberOfVehicles;
+            default -> throw new Exception("Vehicle unknown: " + vehicle);
+        };
+    }
+}
--- src/main/java/blog/softwaretester/ooabusers/switchstatements/SwitchStatements.java
+++
@@ -1,4 +0,0 @@
-package blog.softwaretester.ooabusers.switchstatements;
-
-public class SwitchStatements {
-}
