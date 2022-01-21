e82862305544df777258475414d37ce04640eb81
Benjamin Bischoff <benjamin.bischoff@trivago.com>
2022-01-21 13:54:51 +0100

    data class example

--- src/main/java/blog/softwaretester/dispensables/dataclass/DataClass.java
+++ src/main/java/blog/softwaretester/dispensables/dataclass/DataClass.java
@@ -1,4 +1,11 @@
 package blog.softwaretester.dispensables.dataclass;
 
 public class DataClass {
+    record Rectangle(int sideA, int sideB) {
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 20);
+        int rectangleArea = rectangle.sideA * rectangle.sideB;
+    }
 }
